55a239c629e97879b7e15545be2774772441d1a9
magdeleine <magdeleine.lebrun@gmail.com>
2017-08-18 11:09:35 +0200

    Fix #46 Setup Algolia search bar

--- assets/javascripts/search.js
+++ assets/javascripts/search.js
@@ -1,8 +1,8 @@
 define(['docsearch'], function (docsearch) {
 
   docsearch({
-    apiKey: '6967a402f012b8a7c4122180616aace0',
-    indexName: 'scalingo-doc',
+    apiKey: '5b98d1209a3f82902628f3c46fae4ed1',
+    indexName: 'scalingo',
     inputSelector: '#search-input'
   })
 
