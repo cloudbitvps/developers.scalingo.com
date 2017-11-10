dde74a693dac402b586d59b78ec699d5783a095f
Jonathan Hurter <john@scalingo.com>
2017-11-10 17:47:32 +0100

    Fix integration

--- assets/javascripts/search.js
+++ assets/javascripts/search.js
@@ -1,9 +1,17 @@
 define(['docsearch'], function (docsearch) {
-
-  docsearch({
-    apiKey: '5b98d1209a3f82902628f3c46fae4ed1',
+  var search = docsearch({
+    apiKey: '3f061836c3dbaf1ae75994355b4ce7e2',
     indexName: 'scalingo',
     inputSelector: '#search-input'
+  })
+  $(window).scroll(function() {
+    search.autocomplete.autocomplete.close(); // remove the dropdown
+    search.autocomplete.autocomplete.setVal(''); // clear the input
+  });
+
+  $(".sidebar-nav").scroll(function(){
+    search.autocomplete.autocomplete.close(); // remove the dropdown
+    search.autocomplete.autocomplete.setVal(''); // clear the input
   })
 
 })
